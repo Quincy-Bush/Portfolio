749ecc8e0d16ec1e0670912288a4c4a83cb5981e
Quincy-Bush <quincy.m.bush@gmail.com>
2025-07-15 19:06:57 -0700

    Correcting spacing and wording across html pages
    Adjusted spacing and few wording, while focusing on correcting any spelling errors and mistakes across webpages

--- InteractivePortfolio/src/constants.js
+++ InteractivePortfolio/src/constants.js
@@ -3,13 +3,13 @@
 export const scaleFactor = 2;
 //set of data objects containing content to display during interactions
 export const dialogueData = {
-  bigShelf: ` Shelf full of things that mean a lot to me, I am especially proud of the Computer Science degree  I earned from the <a href="https://youtu.be/owzUS0R7Ny8?t=6242 " target="_blank">University of Minnesota!</a>`,
+  bigShelf: ` This is my shelf full of things that mean a lot to me, I am especially proud of the Computer Science degree I earned from the <a href="https://youtu.be/owzUS0R7Ny8?t=6242 " target="_blank">University of Minnesota!</a>`,
   "drawer": ` Drawer full of previous notebooks and assignments I saved for all my exams. The oldest I have is from <a href="https://quincy-bush.github.io/Portfolio/WebPages/BDPA" target="_blank">Black Data Processing Associates</a>.`,
   "Shelf": ` Shelf of things I really enjoy. Its mostly vinyl records, video games and books I wont read anytime soon.`,
   computer: `The desktop computer, where I have worked on many projects across a vast number of programming languages over many years.
    Here you can take a more detailed look at my skillset and relevent projects I have completed here...
    <a href="https://quincy-bush.github.io/Portfolio/" target="_blank">Resume and Portfolio.</a>,
-   In the future, I will be looking forward to creating more projects accessible to potential employrs alongside the code to this project here...
+   In the future, I will be looking forward to creating more projects accessible to potential employers alongside the code to this project here...
    <a href="https://github.com/Quincy-Bush/" target="_blank">Github Page</a>,`,
   cabinent: `This where I keep my books and drawings I read in college that continue to collect dust. `,
   couch1: `Just another couch I tend to fall asleep on.`,
